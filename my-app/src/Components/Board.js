6f40bae9c898560c2c8ea5af355b0e4063f789b6
Sabaa Taha <sabaataha@Sabaas-MacBook-Pro.local>
2023-06-04 19:33:05 +0300

    built board without reveal action

--- my-app/src/Components/Board.js
+++ my-app/src/Components/Board.js
@@ -2,5 +2,38 @@ import React,{useState,useEffect} from "react";
 import CreateBoard from '../Utils/CreateBoard';
 function Board() {
     const [grid,setGrid]= useState([]);
-    
+    const style={
+        display : 'flex',
+        flexDirection : 'row',
+
+    }
+    useEffect(()=>{
+        function freshBoard(){
+            const newBoard= CreateBoard(10,10,20);
+            setGrid(newBoard);
+        }
+        freshBoard();
+    },[]);
+    return(
+        <div className="parent">
+        {grid.map(singlerow=>{
+            return (
+                <div style={style}>
+                    {singlerow.map(singlecol=>{
+                            return <div 
+                            style={{ 
+                                width : 30,
+                                height : 30,  
+                                padding : '5px' ,
+                                border : '3px solid red'}}
+                                >
+                                {JSON.stringify(singlecol.value)}
+                                </div>
+                    })}
+                </div>
+            )
+        })}
+    </div>
+    )
 }
+export default Board;
