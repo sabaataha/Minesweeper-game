7254b827d63e8e66695c96aff76339c7bc8313dd
Sabaa Taha <sabaataha@Sabaas-MacBook-Pro.local>
2023-06-06 19:55:24 +0300

    built first board

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Board from './Components/Board';
+import {Button,Container} from '@mui/material';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Board />
+    <Board />
     </div>
   );
 }
--- my-app/src/Utils/CreateBoard.js
+++ my-app/src/Utils/CreateBoard.js
@@ -1,93 +1,89 @@
 
-export default function CreateBoard(row,col,bombs){
-    // store values 
-    let board=[];
-    // tracking the minelocation 
-    let mineLocation =[];
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  
 
-    for(let x=0;x<row;x++)
-    {
-        let subCol=[];
-        for(let y=0;y<col;y++)
-        {
+export default CreateBoard (row,col,bombs)
+{
+    let board=[];
+    let minesLocation=[];
+    // create empty board
+    for (let x = 0; x < row; x++) {
+        let subCol = [];
+        for (let y = 0; y < col; y++) {
             subCol.push({
-                value:0,
-                revealed:false,
                 x:x,
                 y:y,
                 flagged:false,
+                revealed:false,
             });
         }
         board.push(subCol);
     }
-    // randomize bomb placement 
-    let bombsCount=0;
-    while(bombsCount<bombs)
+    let bombs_count=0;
+    while(bombs_count<bombs)
     {
-        let x=random(0,row-1);
-        let y=random(0,col-1);
+        let x= getRandomNumber(0,row-1);
+        let y=getRandomNumber(0,col-1) ;
         if(board[x][y].value===0)
         {
             board[x][y].value="X";
-            mineLocation.push([x,y]);
-            bombsCount++;
+            minesLocation.push([x,y]);
+            bombs_count++;
         }
     }
-    for(let i=0;i<row;i++)
-    {
-        for(let j=0;j<col;j++)
-        {
-            if(board[i][j].value==="X")
-            {
-                continue;
-            }
-            //top cell
-            if(i>0 &&board[i-1][j].value==="X")
-            {
-                board[i][j].value++;
-            }
-            // top right 
-            if(j>0 && j<col-1 && board[i-1][j+1].value==="X")
-            {
-                board[i][j].value++;
-            }
-            if(j<col-1 && board[i][j+1].value==="X")
-            {
-                board[i][j].value++;
-            }
-            if(i<row-1 && j<col-1 && board[i+1][j+1]==="X")
-            {
-                board[i][j].value++;
-            }
-            if(i<row-1 && board[i+1][j].value==="X")
-            {
-                board[i][j].value++;
-            }
-              // Bottom Left
+    for (let i = 0; i < row; i++) {
+        for (let j = 0; j < col; j++) {
+          if (board[i][j].value === "X") {
+            continue;
+          }
+          // top 
+          if(i>0 && board[i - 1][j].value === "X"){
+            board[i][j].value++;
+          }
+          // top right 
+          if(i>0 && j<col-1 && board[i-1][j+1].value==="X")
+          {
+            board[i][j].value++;
+          }
+          // top left
+          if(i>0 && j>0 && board[i-1][j-1].value==="X")
+          {
+            board[i][j].value++;
+          }
+          // left
+          if(j>0 && board[i ][j-1].value === "X"){
+            board[i][j].value++;
+          }
+          // right
+          if(j<col-1 && board[i ][j+1].value === "X"){
+            board[i][j].value++;
+          }
+          // Botoom Right
         if (
+            i < row - 1 &&
+            j < col - 1 &&
+            board[i + 1][j + 1].value === "X"
+          ) {
+            board[i][j].value++;
+          }
+
+        
+          // Bottom
+          if (i < row - 1 && board[i + 1][j].value === "X") {
+            board[i][j].value++;
+          }
+    
+          // Bottom Left
+          if (
             i < row - 1 &&
             j > 0 &&
             board[i + 1][j - 1].value === "X"
           ) {
             board[i][j].value++;
           }
-    
-          // LEft
-          if (j > 0 && board[i][j - 1].value === "X") {
-            board[i][j].value++;
-          }
-    
-          // Top Left
-          if (i > 0 && j > 0 && board[i - 1][j - 1].value === "X") {
-            board[i][j].value++;
-          }
         }
-        
     }
-    return { board, mineLocation };
-
+    return { board, minesLocation };
 };
-function random(min = 0, max) {
-    // min and max included
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
